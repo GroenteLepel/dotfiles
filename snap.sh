c7bdcce6b791da3bd0d241cb351389aec6604f41
Daniel <github@danieljkok.nl>
2025-07-11 15:39:41 +0200

    Add Ubuntu snap installation file.
    Signed-off-by: Daniel <github@danieljkok.nl>

---
+++ snap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+snap install signal-desktop
+snap install firefox
+snap install mailspring
+snap install vlc
+snap install transmission
+snap install obsidian
+snap install discord
