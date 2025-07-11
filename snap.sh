bb4b0e4f5a1c61a1c15aca0a2339e308243ae367
Daniel <github@danieljkok.nl>
2025-07-11 16:06:53 +0200

    Add apt installations and configure syncthing.
    Signed-off-by: Daniel <github@danieljkok.nl>

--- snap.sh
+++ snap.sh
@@ -3,6 +3,13 @@
 apt update
 apt upgrade
 
+# Add the syncthing candidate to APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
+apt install syncthing
+cp /usr/share/applications/syncthing-start.desktop ~/.config/autostart/
+
+apt install ffmpeg
+
 snap install signal-desktop
 snap install firefox
 snap install mailspring
