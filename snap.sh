3ab8fc4921838051d591863297c007e9fdeb1b3f
Daniel <github@danieljkok.nl>
2025-09-27 14:46:00 +0200

    check if autostart folder exists.
    Signed-off-by: Daniel <github@danieljkok.nl>

--- snap.sh
+++ snap.sh
@@ -6,6 +6,9 @@ apt upgrade
 # Add the syncthing candidate to APT sources:
 echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
 apt install syncthing
+if [ ! -d /home/.config/autostart/ ]; then
+  mkdir -p /home/.config/autostart/;
+fi
 cp /usr/share/applications/syncthing-start.desktop ~/.config/autostart/
 
 apt install ffmpeg
