09915aceb0b95e8ec31da59742cdba27293f58e7
Daniel <github@danieljkok.nl>
2026-06-28 11:59:17 +0200

    feat: add gnome-tiling-manager plugin to Ubuntu installation.
    Signed-off-by: Daniel <github@danieljkok.nl>

--- snap.sh
+++ snap.sh
@@ -3,8 +3,18 @@
 apt update
 apt upgrade
 
-# Add the syncthing candidate to APT sources:
-echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
+# Install syncthing
+# Add the release PGP keys:
+if [ ! -d /etc/apt/keyrings/ ]; then
+  mkdir -p /etc/apt/keyrings/;
+fi
+apt install curl
+curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+
+# Add the "stable-v2" channel to your APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable-v2" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+apt update
 apt install syncthing
 if [ ! -d /home/.config/autostart/ ]; then
   mkdir -p /home/.config/autostart/;
@@ -12,6 +22,15 @@ fi
 cp /usr/share/applications/syncthing-start.desktop ~/.config/autostart/
 
 apt install ffmpeg
+
+# GNOME tiling manager
+apt install -y gettext
+git clone git@github.com:ubuntu/Tiling-Assistant.git /tmp/tiling-assistant
+cd /tmp/tiling-assistant
+./scripts/build.sh -i
+cd -
+rm -rf /tmp/tiling-assistant
+
 apt install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
